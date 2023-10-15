454f8166fb5b6e3b1436a10f18992fcf4f4b6a40
pkafei <plburton@gmail.com>
2023-10-15 13:33:09 -0400

    Using Langchain agents

--- langchain_helper.py
+++ langchain_helper.py
@@ -1,6 +1,9 @@
 from langchain.llms import OpenAI
 from langchain.chains import LLMChain
 from dotenv import load_dotenv
+from langchain.agents import load_tools
+from langchain.agents import initialize_agent
+from langchain.agents import AgentType 
 
 load_dotenv()
 
@@ -11,5 +14,21 @@ def generate_pet_name(pet_name, color):
 
     return name
 
+def langchain_agent():
+
+    llm = OpenAI(temperature=0.5)
+
+    tools = load_tools(["wikipedia", "llm-math"], llm = llm)
+
+    agent = initialize_agent(
+        tools, llm, agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION, verbose=True
+    )
+
+    result = agent.run(
+        "What is the average age of a dog? Multiple the age by 2",
+    )
+
+
 if __name__ == "__main__":
-    print(generate_pet_name())
+    langchain_agent()
+    # print(generate_pet_name())
